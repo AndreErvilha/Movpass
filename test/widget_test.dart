42014cdffa6089192248ffd768ce22efbf1678eb
ervil <andre.eduardo.ervilha@gmail.com>
2021-01-18 23:56:37 -0300

    Created an example of how create a widget test see 'widget_test.dart' file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,24 +7,21 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:movpass/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Verify alert message on press fab', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify the alertDialog is not on screen
+    expect(find.text('Mensagem de alerta'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    // Tap the 'message' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.message));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('2'), findsOneWidget);
+    // Verify if alertDialog is showed with a text 'Mensagem de alerta'
+    expect(find.text('Mensagem de alerta'), findsOneWidget);
   });
 }
